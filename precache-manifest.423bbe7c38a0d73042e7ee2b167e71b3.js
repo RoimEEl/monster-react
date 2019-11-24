dbd1ed7be2f95b9da25d84b6c7bec539b08eb180
RoimEEl <41959880+RoimEEl@users.noreply.github.com>
2019-11-24 10:03:10 +0800

    Updates

---
+++ precache-manifest.423bbe7c38a0d73042e7ee2b167e71b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db86a6e4d4fb840563e4eb060ed12ace",
+    "url": "/monster-react/index.html"
+  },
+  {
+    "revision": "d315ac14198b28948740",
+    "url": "/monster-react/static/css/main.0b006061.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monster-react/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "d315ac14198b28948740",
+    "url": "/monster-react/static/js/main.e2e5f805.chunk.js"
+  },
+  {
+    "revision": "f079812b1addbeadb1a4",
+    "url": "/monster-react/static/js/runtime-main.c1dc7fc1.js"
+  }
+]);
